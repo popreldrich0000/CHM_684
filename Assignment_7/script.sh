3c11765493bfc1f26b820ac849fdb8d95928bce1
Shubham Jangid <84173745+popreldrich0000@users.noreply.github.com>
2023-03-25 11:13:19 +0000

    starting

---
+++ Assignment_7/script.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+gfortran -o run function1.f95 function2.f95 function3.f95 function4.f95 main.f95
+
+
+./run
--- Assignment_7/shubham.py
+++
@@ -1,5 +0,0 @@
-import random
-
-print(random.randint(0,255))
-print("Do din ke liye chalaunga phir aaram karunga")
-print("mai chutiya hu")
